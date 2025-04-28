bccde0e9f2c15501bb6a494327a06a2e7f41a256
AlsoPeters <kyle.fire@gmail.com>
2025-04-28 14:41:42 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import './App.css'
+import Dice from './components/Dice'
+import { useState } from 'react'
+
+
+function App() {
+  const [diceValue, setDiceValue] = useState(1)
+
+  // Fisher-Yates Shuffle Algorithm
+  function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]]
+    }
+  }
+
+  function HandleRoll() {
+    let sides = [1, 2, 3, 4, 5, 6]
+    shuffleArray(sides)
+    setDiceValue(sides[0])
+  }
+
+  return (
+    <>
+      <div className='flex justify-center items-center space-y-6 flex-col'>
+        <button onClick={HandleRoll} className=' text-blue-400 text-4xl border-2 p-2 rounded-md cursor-pointer hover:bg-gray-800'>Roll</button>
+        <div className='flex flex-row space-x-6'>
+          <Dice value={diceValue} />
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Dice.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Dice = ({ value = 1 }) => {
+  const renderDots = (value) => {
+    const dots = [];
+    
+    const dotPositions = {
+      1: [[50, 50]],
+      2: [[25, 25], [75, 75]],
+      3: [[25, 25], [50, 50], [75, 75]],
+      4: [[25, 25], [25, 75], [75, 25], [75, 75]],
+      5: [[25, 25], [25, 75], [50, 50], [75, 25], [75, 75]],
+      6: [[25, 25], [25, 50], [25, 75], [75, 25], [75, 50], [75, 75]],
+    };
+
+    (dotPositions[value] || []).forEach(([cx, cy]) => {
+      dots.push(<circle key={`${cx}-${cy}`} cx={cx} cy={cy} r="8" fill="black" />);
+    });
+    return dots;
+  };
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" fill="white" stroke="black" strokeWidth="5">
+      <rect width="100" height="100" rx="15" ry="15" fill="white" />
+      {renderDots(value)}
+    </svg>
+  );
+};
+
+export default Dice;
